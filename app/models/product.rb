d6936b05413d27d179f1f243f17d5510c35a06a3
Airton Ponce <airtonponce@outlook.com>
2018-12-04 09:23:40 -0200

    add product model

---
+++ app/models/product.rb
@@ -0,0 +1,6 @@
+class Product < ApplicationRecord
+  belongs_to :user
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
+  validates :quantity, presence: true, numericality: { only_integer: true }
+end
