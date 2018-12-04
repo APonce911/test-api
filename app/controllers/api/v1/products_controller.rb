5afbbdfbbba9180b837c44a9e27838ea31535a61
Airton Ponce <airtonponce@outlook.com>
2018-12-04 12:19:44 -0200

    add update route and controller & add uniqueness validation to product description

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ProductsController < Api::V1::BaseController
-  # before_action :set_product, only: [:show]
+  before_action :set_product, only: [:show, :update]
 
   def index
     @products = policy_scope(Product)
@@ -7,12 +7,30 @@ class Api::V1::ProductsController < Api::V1::BaseController
 
 
   def show
-    @product = Product.find(params[:id])
-    authorize @product # For Pundit
+  end
+
+  def update
+    if @product.update(prodcut_params)
+      render :show
+    else
+      render_error
+    end
   end
 
   private
 
-  def set_product
+  def product_params
+    params.require(:product).permit(:description, :price, :quantity)
   end
+
+  def set_product
+    @product = Product.find(params[:id])
+    authorize @product # For Pundit
+  end
+
+  def render_error
+    render json: { errors: @product.errors.full_messages },
+      status: :unprocessable_entity
+  end
+
 end
