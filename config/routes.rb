f07f4da8b8954867cd11441e3a67b7502000f9b3
Airton Ponce <airtonponce@outlook.com>
2018-12-05 19:14:36 -0200

    redirect from '/' to api/v1/products#index

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
     end
   end
 
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root to:'api/v1/products#index', defaults: { format: :json }
+  get '/', to: 'pages#home'
 
 
 end
