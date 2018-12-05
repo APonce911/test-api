afcf7fe9deabca9e8d7a78115531e274e3744349
Airton Ponce <airtonponce@outlook.com>
2018-12-05 13:46:09 -0200

    email user after creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :products
   validates :name, presence: true
+
+  after_create :send_welcome_email
+
+  private
+
+  def send_welcome_email
+    UserMailer.welcome(self).deliver_now
+  end
+
 end
